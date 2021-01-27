1d6985d6aa5bef5275b0be59daa0650890411608
Maluki Muthusi <malukimuthusi@yahoo.com>
2021-01-27 14:01:40 +0300

    Feat: Make home address always on top of the favourite list. (#9)
    Add popup menu to the home address item. Reset the home address item
    through the popup menut. Can also select to edit the home address.
    
    Also add popup menu for the other items in the favourite list. Can
    select to remove an item from the favourite item. Can also edit an item
    in the favourite list.

--- app/src/main/java/codes/malukimuthusi/safiri/FavoriteAdapter.kt
+++ app/src/main/java/codes/malukimuthusi/safiri/FavoriteAdapter.kt
@@ -3,13 +3,17 @@ package codes.malukimuthusi.safiri
 import android.content.pm.PackageManager
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import codes.malukimuthusi.safiri.databinding.FavouriteHeaderBinding
-import codes.malukimuthusi.safiri.databinding.FavouriteListLayoutBinding
+import codes.malukimuthusi.safiri.databinding.FavouriteListSingleLayoutBinding
+import codes.malukimuthusi.safiri.databinding.HomeFavouriteBinding
 import codes.malukimuthusi.safiri.databinding.LocationSelectorBinding
 import codes.malukimuthusi.safiri.models.Address
 import com.google.android.material.snackbar.Snackbar
@@ -19,13 +23,20 @@ import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions
 import com.mapbox.mapboxsdk.plugins.places.picker.PlacePicker
 import com.mapbox.mapboxsdk.plugins.places.picker.model.PlacePickerOptions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class FavoriteAdapter(private val mainActivity: HomeFragment) :
+class FavoriteAdapter(
+    private val homeFragment: HomeFragment,
+    private val viewModel: HomeViewModel
+) :
     ListAdapter<Address, RecyclerView.ViewHolder>(FavoriteDIFF) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             1 -> LocationSelectorViewHolder.init(parent)
             2 -> FavoriteHeaderViewHolder.init(parent)
+            3 -> HomeFavoriteViewHolder.init(parent)
             else -> FavoriteViewHolder.init(parent)
         }
     }
@@ -36,17 +47,21 @@ class FavoriteAdapter(private val mainActivity: HomeFragment) :
             1 -> {
                 // select location layout
                 holder as LocationSelectorViewHolder
-                holder.bind(mainActivity)
+                holder.bind(homeFragment)
             }
             2 -> {
                 // favourite list header
                 holder as FavoriteHeaderViewHolder
-                holder.bind(mainActivity)
+                holder.bind(homeFragment)
+            }
+            3 -> {
+                holder as HomeFavoriteViewHolder
+                holder.bind(getItem(position), homeFragment, viewModel)
             }
             else -> {
                 // favourite list
                 holder as FavoriteViewHolder
-                holder.bind(getItem(position))
+                holder.bind(getItem(position), homeFragment, viewModel)
             }
         }
     }
@@ -55,6 +70,7 @@ class FavoriteAdapter(private val mainActivity: HomeFragment) :
         return when (position) {
             0 -> 1
             1 -> 2
+            2 -> 3
             else -> super.getItemViewType(position)
         }
 
@@ -71,17 +87,48 @@ object FavoriteDIFF : DiffUtil.ItemCallback<Address>() {
     }
 }
 
-class FavoriteViewHolder(private val view: FavouriteListLayoutBinding) :
-    RecyclerView.ViewHolder(view.root) {
-    fun bind(favoriteItem: Address) {
-        view.address.text = favoriteItem.LongName
+class FavoriteViewHolder(private val binding: FavouriteListSingleLayoutBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(favoriteItem: Address, homeFragment: HomeFragment, viewModel: HomeViewModel) {
+        binding.address.text = favoriteItem.LongName
+
+        binding.more.setOnClickListener { moreImageView ->
+            val popupMenu = PopupMenu(moreImageView.context, moreImageView)
+            popupMenu.menuInflater.inflate(R.menu.more_menu, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.title) {
+                    moreImageView.context.getString(R.string.remove) -> {
+                        homeFragment.lifecycleScope.launch {
+                            withContext(Dispatchers.Main) {
+                                viewModel.removeItem(favoriteItem)
+                            }
+                        }
+                        true
+                    }
+                    else -> {
+                        Toast.makeText(
+                            moreImageView.context,
+                            "Please Edit Item",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        true
+                    }
+                }
+
+            }
+            popupMenu.setOnDismissListener {
+                // TODO: Handle item dismiss
+            }
+            popupMenu.show()
+        }
     }
 
     companion object {
         fun init(parent: ViewGroup): FavoriteViewHolder {
 
             val binding =
-                FavouriteListLayoutBinding.inflate(
+                FavouriteListSingleLayoutBinding.inflate(
                     LayoutInflater.from(parent.context),
                     parent,
                     false
@@ -126,7 +173,7 @@ class FavoriteHeaderViewHolder(private val view: FavouriteHeaderBinding) :
                     ).show()
                 }
                 else -> {
-                    mainActivity.requestPermisLctPickLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
+                    mainActivity.requestPermissionLocationPickLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
                 }
             }
 
@@ -182,7 +229,7 @@ class LocationSelectorViewHolder(private val view: LocationSelectorBinding) :
                 }
 
                 else -> {
-                    homeFragment.requestPermisLctSelectLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
+                    homeFragment.requestPermissionLocationSelectLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
                 }
             }
         }
@@ -221,7 +268,7 @@ class LocationSelectorViewHolder(private val view: LocationSelectorBinding) :
                 }
 
                 else -> {
-                    homeFragment.requestPermisLctPickLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
+                    homeFragment.requestPermissionLocationPickLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
                 }
             }
         }
@@ -236,6 +283,96 @@ class LocationSelectorViewHolder(private val view: LocationSelectorBinding) :
                     false
                 )
             return LocationSelectorViewHolder(binding)
+        }
+    }
+}
+
+// home item
+class HomeFavoriteViewHolder(private val view: HomeFavouriteBinding) :
+    RecyclerView.ViewHolder(view.root) {
+    fun bind(favoriteItem: Address, homeFragment: HomeFragment, viewModel: HomeViewModel) {
+        view.address.text = favoriteItem.LongName
+        view.placeName.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    homeFragment.requireActivity(),
+                    android.Manifest.permission.READ_PHONE_STATE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    val placePickerOptions = PlacePickerOptions.builder()
+                        .statingCameraPosition(
+                            CameraPosition.Builder()
+                                .target(LatLng(-1.2921, 36.8219))
+                                .zoom(16.0)
+                                .build()
+                        )
+                        .build()
+                    val intent = PlacePicker.IntentBuilder()
+                        .accessToken(homeFragment.getString(R.string.MapboxAccessToken))
+                        .placeOptions(placePickerOptions)
+                        .build(homeFragment.requireActivity())
+                    homeFragment.pickHomeLocationLauncher.launch(intent)
+                }
+
+                shouldShowRequestPermissionRationale(
+                    homeFragment.requireActivity(),
+                    android.Manifest.permission.READ_PHONE_STATE
+                ) -> {
+                    Snackbar.make(
+                        view.root,
+                        "please provide required permissions",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+
+                else -> {
+                    homeFragment.requestPermissionPickHomeLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
+                }
+            }
+        }
+
+        view.more.setOnClickListener { moreImageView ->
+            val popMenu = PopupMenu(moreImageView.context, moreImageView)
+            popMenu.menuInflater.inflate(R.menu.home_more_menu, popMenu.menu)
+
+            popMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.title) {
+                    moreImageView.context.getString(R.string.home_reset) -> {
+                        homeFragment.lifecycleScope.launch {
+                            withContext(Dispatchers.Main) {
+                                viewModel.removeItem(favoriteItem)
+                            }
+                        }
+                        true
+                    }
+                    else -> {
+                        Toast.makeText(
+                            moreImageView.context,
+                            "Please Edit Item",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        true
+                    }
+                }
+            }
+
+            popMenu.setOnDismissListener {
+                // TODO: handle menu dismiss
+
+            }
+            popMenu.show()
+
+        }
+    }
+
+    companion object {
+        fun init(parent: ViewGroup): HomeFavoriteViewHolder {
+            val binding =
+                HomeFavouriteBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            return HomeFavoriteViewHolder(binding)
         }
     }
 }
